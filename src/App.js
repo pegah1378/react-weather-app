b6570a9e0a3aa7b150bc952dee28a4872eb50e13
pegah <100560899+pegah1378@users.noreply.github.com>
2022-04-28 14:59:40 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 export default function App() {
-  return <Weather defualtCity="London" />;
+  return <Weather defaultCity="London" />;
 }
