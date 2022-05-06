e15db960e82c29c19b5cd9eeaaba40e0c875cb97
pegah <100560899+pegah1378@users.noreply.github.com>
2022-05-06 20:52:54 +0430

    app.css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="New York" />
+        <br />
 
         <footer>
           This project was coded by{" "}
