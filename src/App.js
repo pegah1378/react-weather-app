0d555449e7bab4a9042bc93bda43edbe6f0ff001
pegah <100560899+pegah1378@users.noreply.github.com>
2022-05-06 20:49:31 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from "react";
-import "./App.css";
 import Weather from "./Weather";
+import "./App.css";
+
 export default function App() {
-  return <Weather defaultCity="London" />;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a href="mailto:pegah1251@gmail.com" target="_blank">
+            Pegah Moradpour
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/pegah1378/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="https://weather-app-by-pegah.netlify.app/" target="_blank">
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
